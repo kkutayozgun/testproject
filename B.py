80a843a04f744baa0324f86b7e11c63905611a28
kkutayozgun <kkutayozgun@gmail.com>
2025-02-17 16:02:51 +0300

    degisiklik

--- B.py
+++ B.py
@@ -1,4 +1,7 @@
 def to_uppercase(input_string):
-    print("kutay")
+    print("kutay was here")
     return input_string.upper()
     
+
+def to_lowercase(input_string):
+    return input_string.lower()
